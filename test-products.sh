cdfd803006f6389f2be9241bc32e508f122d8cd0
aa1588 <amritcsadhikari@gmail.com>
2025-11-17 20:56:01 -0600

    update test script

--- test-products.sh
+++ test-products.sh
@@ -5,19 +5,19 @@ echo "=== Creating Products ==="
 # Product 1
 curl -X POST http://localhost:3000/products \
   -H "Content-Type: application/json" \
-  -d '{"name":"Laptop","price":1200.50}'
+  -d '{"name":"Laptop","price":"1200.50"}'
 echo -e "\n"
 
 # Product 2
 curl -X POST http://localhost:3000/products \
   -H "Content-Type: application/json" \
-  -d '{"name":"Phone","price":899.99}'
+  -d '{"name":"Phone","price":"899.99"}'
 echo -e "\n"
 
 # Product 3
 curl -X POST http://localhost:3000/products \
   -H "Content-Type: application/json" \
-  -d '{"name":"Headphones","price":199.00}'
+  -d '{"name":"Headphones","price":"199.00"}'
 echo -e "\n"
 
 echo "=== Getting All Products ==="
@@ -31,7 +31,7 @@ echo -e "\n"
 echo "=== Updating Product 2 ==="
 curl -X PUT http://localhost:3000/products/2 \
   -H "Content-Type: application/json" \
-  -d '{"name":"Updated Laptop","price":1400.00}'
+  -d '{"name":"Updated Laptop","price":"1400.00"}'
 echo -e "\n"
 
 echo "=== Deleting Product 2 ==="
