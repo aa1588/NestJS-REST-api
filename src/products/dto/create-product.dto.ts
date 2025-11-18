77c7b6509d02cd7b12c6ad4a6e8a818d86ce1357
aa1588 <amritcsadhikari@gmail.com>
2025-11-17 20:55:17 -0600

    add dto validation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,5 +1,12 @@
+import { IsString, IsOptional } from 'class-validator';
+
 export class CreateProductDto {
+    @IsString()
     name: string;
+    @IsOptional()
+    @IsString()
     description?: string;
+
+    @IsString()
     price: string; // send as string to avoid float precision issues
 }
